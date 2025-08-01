f13fa4c62eb1b37caae212579c292b86ec5d1dca
noahlio <noahbobis0512@gmail.com>
2025-08-01 23:03:35 +0200

    añadir specs guard nueva entidad polimorfica comments seeds automaticos de fixtures ficheros xml invoice prueba fix warnings wickedpdf.config

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,4 @@
 class Client < ApplicationRecord
     has_many :invoices
+    has_many :comments, as: :commentable, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "invalid email format" }
-
   has_many :invoices, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "invalid email format" }
 end
